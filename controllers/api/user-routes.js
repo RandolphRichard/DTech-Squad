57b42f017893fda966b1190e22ca0895e9533a5d
Randolph Richard <randolphrichard9@gmail.com>
2021-11-27 14:56:07 -0500

    The App is working fine

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -81,7 +81,7 @@ router.post('/', (req, res) => {
       }
     }).then(dbUserData => {
       if (!dbUserData) {
-        res.status(400).json({ message: 'There was no User with that email address!' });
+        res.status(400).json({ message: 'There was no User with that email address' });
         return;
       }
   
@@ -127,7 +127,7 @@ router.put('/:id', withAuth, (req, res) => {
     })
       .then(dbUserData => {
         if (!dbUserData[0]) {
-          res.status(404).json({ message: 'There was no User under this id' });
+          res.status(404).json({ message: 'No user found with this id' });
           return;
         }
         res.json(dbUserData);
